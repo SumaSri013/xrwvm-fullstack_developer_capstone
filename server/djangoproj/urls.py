84366bf2edd0444519bbb8e62d344325b0e100b8
SumaSri013 <lingampallysumasri@gmail.com>
2026-03-15 19:33:01 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
